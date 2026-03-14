8565a3271b1a16e521725b5857961ca4aa43b050
Shahna-Sherin <shahnasherin2077@gmail.com>
2026-03-15 03:36:22 +0530

    Fix MongoDB SSL for Render

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -1,15 +1,20 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
+import ssl
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
-client = AsyncIOMotorClient(MONGO_URI)
-db = client[MONGO_DB_NAME]
+client = AsyncIOMotorClient(
+    MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
 
+db = client[MONGO_DB_NAME]
 donors_collection = db["donors"]
 requests_collection = db["blood_requests"]
 users_collection = db["users"]
