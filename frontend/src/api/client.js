043b6d0a116385b8b3d0fc0a630309c5e7e92558
Shahna-Sherin <shahnasherin2077@gmail.com>
2026-03-15 02:10:16 +0530

    Update API URL to Render backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://bloodlink-ftzc.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
